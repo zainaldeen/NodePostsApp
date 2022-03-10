0b80225f824aba1e0de7b28f7157cef6bed4ff24
Zain AlDeen Fayod <zainaldeenfayod@gmail.com>
2022-03-10 07:02:23 -0500

    add JWT to all requests

--- routes/posts.js
+++ routes/posts.js
@@ -6,13 +6,15 @@ const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
 router.get('/posts', isAuth, postController.getPosts);
-router.get('/posts/:postId', postController.getPostById);
+router.get('/posts/:postId', isAuth, postController.getPostById);
 router.post('/post', [
-    body('title').trim().isLength({min: 5}),
-    body('content').trim().isLength({min: 5}),
-], postController.postPosts);
+        body('title').trim().isLength({min: 5}),
+        body('content').trim().isLength({min: 5}),
+    ],
+    isAuth,
+    postController.postPosts);
 
-router.put('/post/:postId', postController.updatePost);
-router.delete('/post/:postId', postController.deletePost);
+router.put('/post/:postId', isAuth, postController.updatePost);
+router.delete('/post/:postId', isAuth, postController.deletePost);
 
 module.exports = router;
