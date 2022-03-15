094a12349174bb00e2d2e6d876f7c89efef31886
Zain AlDeen Fayod <zainaldeenfayod@gmail.com>
2022-03-15 09:28:47 -0400

    login via graphql

--- graphQl/resolver.js
+++ graphQl/resolver.js
@@ -1,8 +1,11 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+
 module.exports = {
     createUser: async function ({ userInput }, req){
+        console.log(userInput);
         let errors = [];
         if (!validator.isEmail(userInput.email)) {
             errors.push({ message: 'Invalid Email'});
@@ -31,6 +34,36 @@ module.exports = {
         return {
             ...createdUser._doc,
             _id: createdUser._id.toString(),
+        }
+    },
+
+    login: async function ({ loginData }, req) {
+        let error = [];
+        const user = await User.findOne({email: loginData.email});
+        if (!user) {
+            const error = new Error("Check your credentials");
+            error.code = 401;
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(loginData.password, user.password);
+        if (!isEqual){
+            const error = new Error("Check your credentials");
+            error.code = 401;
+            throw error;
+        }
+        const token = await jwt.sign({
+            email: user.email,
+            userId: user._id.toString()
+        },
+            'longTokenTpMakeTheJWTSECRETASPOSSIBLE!!OK?',
+            {
+                expiresIn: '1h'
+            }
+        )
+
+        return {
+            userId: user._id.toString(),
+            token: token
         }
     }
 }
